c7552831ed248fd1f2895e2b856efcba1a813261
Carmen López <calope03@ucm.es>
2018-02-15 16:05:26 +0100

    puesto en su sitio el .js

--- src/practica1.js
+++ src/practica1.js
@@ -7,8 +7,35 @@ var MemoryGame = MemoryGame || {};
 
 /**
  * Constructora de MemoryGame
+
+ Esta clase guarda un array con las cartas y el estado en el que se encuentra el juego en
+cada momento, por lo que es la responsable de decidir cuándo ha terminado el juego.
+También guarda el mensaje que aparece en pantalla y que se irá cambiando a medida
+que interactuamos con el juego. También ha de tener una referencia al servidor gráfico
+para poder dibujar el estado del juego. Esta clase ha de implementar al menos los
+siguientes métodos
  */
 MemoryGame = function(gs) {
+
+	this.arrayCartas = [];
+	
+
+	this.initGame = function () {
+		console.log("hola");
+		// for (i = 0; i < 16; i+=2) { 
+		//     this.arrayCartas[i] = gs.maps["guy"];
+		//     this.arrayCartas[i+1] = gs.maps["guy"];
+		// }
+		
+	};
+
+	/*this.onClick = function () {
+		console.log("Hola");
+	};*/
+
+	/*this.initGame = function () {
+		console.log("Hola");
+	};*/
 
 };
 
@@ -21,5 +48,16 @@ MemoryGame = function(gs) {
  * @param {string} id Nombre del sprite que representa la carta
  */
 MemoryGameCard = function(id) {
+	this.estado = "bocabajo";
+	this.valor = id;
+
+	this.flip = function () {
+		if(this.estado == "bocabajo"){
+			this.estado = "bocarriba";
+		}
+		else if(this.estado == "bocarriba"){
+			this.estado = "bocabajo";
+		}
+	};
 
 };
