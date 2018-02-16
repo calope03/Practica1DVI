b57dcea56641a616003555c67dac769713efb59f
Carmen López <calope03@ucm.es>
2018-02-16 21:38:35 +0100

    resuelta la logica de voltear y demas
    faltaria ver lo de mezclar y anular la entrada de raton mientras se juega

--- src/practica1.js
+++ src/practica1.js
@@ -19,6 +19,9 @@ MemoryGame = function(gs) {
 
 	this.arrayCartas = [];
 	this.grafic = gs;
+	this.cartavolteada =0;
+	this.estadopartida = "Memory Game";
+	this.cartasEncontradas = 0;
 	this.initGame = function () {
 		this.cartasPosibles =["8-ball","dinosaur","guy","kronos","potato","rocket","unicorn","zeppelin"];
 		for (i = 0; i < 16; i+=2) { 
@@ -29,17 +32,59 @@ MemoryGame = function(gs) {
 	};
 
 	this.loop = function () {
-		setInterval(this.draw,10000);
+		//console.log(this.arrayCartas[0].valor);
+		setInterval(this.draw.bind(this),16);
 	};
 
 	this.draw = function () {
-		console.log(arrayCartas[0].valor);
+		//console.log(this.arrayCartas[0].valor);
 		//this.grafic.draw(this.arrayCartas[0].valor,0);
-		/*for (i = 0; i < 16; i++) { 
-
-			this.grafic.draw(this.arrayCartas[i].valor,i);
-		}*/
+		this.grafic.drawMessage(this.estadopartida);
+		for (i = 0; i < 16; i++) { 
+			if(this.arrayCartas[i].estado == "bocabajo"){
+				this.grafic.draw("back",i);
+			}else{
+				this.grafic.draw(this.arrayCartas[i].valor,i);
+			}
+			
+		}
 	};
+	/*
+	Este método se llama cada vez que el jugador pulsa sobre alguna de las cartas (identificada por el número que ocupan en el array de cartas del juego). Es el responsable de voltear la carta y, si hay dos volteadas, comprobar si son la misma (en cuyo caso las marcará como encontradas). En caso de no ser la misma las volverá a poner boca abajo1.
+	*/
+	this.onClick = function (cardID) {
+		this.carta=cardID;
+		this.arrayCartas[this.carta].flip();
+		this.cartavolteada++;
+		this.posotraCarta;
+		if(this.cartavolteada == 2){
+			for (i = 0; i < 16; i++) { 
+				if((this.arrayCartas[i].estado=="bocarriba")&&(this.arrayCartas[this.carta]!= this.arrayCartas[i])){
+					this.posotraCarta = i;
+				}		
+			}
+			this.arrayCartas[this.carta].compareTo(this.arrayCartas[this.posotraCarta]);
+			if(this.arrayCartas[this.carta].estado!="encontrada"){
+				setTimeout(volteardos.bind(this), 1000);
+				this.estadopartida="Try Again";
+				/*this.arrayCartas[this.carta].flip();
+				this.arrayCartas[posotraCarta].flip();*/
+			}else{
+				this.cartasEncontradas+=2;
+				this.estadopartida="Match Found";
+			}
+			this.cartavolteada = 0;
+		}
+		if(this.cartasEncontradas==16){
+			this.estadopartida="You Win";
+		}
+		volteardos = function(){
+			this.arrayCartas[this.carta].flip();
+			this.arrayCartas[this.posotraCarta].flip();
+		};
+		
+	};
+
 
 };
 
@@ -63,4 +108,15 @@ MemoryGameCard = function(id) {
 		}
 	};
 
+	this.compareTo = function (otherCard) {
+		carta1=this;
+		carta2=otherCard;
+		if(carta1.valor==carta2.valor){
+			carta1.found();
+			carta2.found();
+		}
+	};
+	this.found = function() {
+		this.estado = "encontrada";
+	}
 };
